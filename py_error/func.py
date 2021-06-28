f0b7efcc1ca50469786dfa5c25afb7acd7d7e1a4
Alex R <i@sepa.spb.ru>
2021-06-28 12:43:48 +0300

    Add external deps

--- py_error/func.py
+++ py_error/func.py
@@ -1,15 +1,23 @@
 #!/usr/bin/env python3
-import sys
+import sys 
+import requests
+import psycopg2
 
+requests.post('https://httpbin.org/post', allow_redirects='fail')
+
+try:
+  pass
+except psycopg2.OperationalError as e:
+  print('ok')
+  
 class smallcase:
     def CapsMethod(self, n, d=2):
-        if n % d == 0:
-            print(f'NO = {d}')
-            return
-        if d > n/2:
-            print(f'YES = {d}')
-            return
-        self.CapsMethod(n, d+1)
-
+          if n % d == 0:
+              print(f'NO = {d}')
+              return
+          if d > n/2:
+              print(f'YES = {d}')
+              return
+          self.CapsMethod(n, d+1)
 
 smallcase().CapsMethod(10)
