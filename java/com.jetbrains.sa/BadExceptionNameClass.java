4c4aa8a42dd2bea2618b8647ed016a8e4fa9df36
Alexander Ryabov <alexander.ryabov@jetbrains.com>
2021-08-18 15:50:07 +0300

    add java failed inspection example

--- java/com.jetbrains.sa/BadExceptionNameClass.java
+++ java/com.jetbrains.sa/BadExceptionNameClass.java
@@ -3,3 +3,12 @@ package com.jetbrains.sa;
 @SuppressWarnings("unused")
 public class BadExceptionNameClass extends RuntimeException {
 }
+
+// Condition is always true
+class Main {
+    public static void main(String[] args) {
+        if (true) {
+
+        }
+    }
+}
